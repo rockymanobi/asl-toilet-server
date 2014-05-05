16a2496583a5343f1a4c4e80928ad5143f0927bc
rocky_manobi <tak.koroki@gmail.com>
2014-05-05 23:42:01 +0900

    statusに日付を入れるように修正

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,9 +1,12 @@
 
 class DevicesController < ApplicationController
   before_action :set_device, only: [:heart_beat]
+
   def heart_beat
-    p 'hoge'
-    render json: nil
+    @device.status = "last_beat_at" + Time.now.strftime( "%Y/%m/%d-%H:%M:%S" )
+    @device.save!
+
+    render json: @device
   end
 
   private
