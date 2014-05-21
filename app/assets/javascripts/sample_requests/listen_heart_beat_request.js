e940924ed61c562e757f0145795467f161bcf40e
rocky_manobi <tak.koroki@gmail.com>
2014-05-22 01:32:06 +0900

    heart_beatリスニングを20秒に１回に修正

--- app/assets/javascripts/sample_requests/listen_heart_beat_request.js
+++ app/assets/javascripts/sample_requests/listen_heart_beat_request.js
@@ -2,5 +2,5 @@ window.listen_heart_beat_request = function(){
   $.post('/sample_requests/listen_heart_beat')
     .done( function(){ console.log("ok"); })
     .fail( function(){ console.log("ng"); })
-    .always( setTimeout( listen_heart_beat_request, 2000 ) );
+    .always( setTimeout( listen_heart_beat_request, 20000 ) );
 };
