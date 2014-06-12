e070a606671ac0e5891038efd08494c2d5e53d64
rocky_manobi <tak.koroki@gmail.com>
2014-06-12 18:54:29 +0900

    ログのapiパラメータにstall_nameを追加

--- app/controllers/test_logs_controller.rb
+++ app/controllers/test_logs_controller.rb
@@ -3,9 +3,11 @@ class TestLogsController < ApplicationController
   def index
 
     target_date_yyyymmdd = params[:date_yyyymmdd]
+    target_stall_name = params[:stall_name]
 
     @logs = TestLog.all
     @logs = @logs.date_at( Date.parse(target_date_yyyymmdd) ) if target_date_yyyymmdd.present?
+    @logs = @logs.where!( stall_name: target_stall_name ) if target_stall_name.present?
 
   end
 end
